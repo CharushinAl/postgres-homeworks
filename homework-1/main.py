0f2916ea90ecbefa0873c963a50f99004d23f7d0
Aleksandr Charushin <charushinas@gmail.com>
2023-07-25 16:03:37 +0300

    Using context manager "with" and try block

--- homework-1/main.py
+++ homework-1/main.py
@@ -18,21 +18,26 @@ with open('north_data/orders_data.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     orders = [tuple(row) for row in reader if row[0].isnumeric()]
 
-with psycopg2.connect(
+conn = psycopg2.connect(
     host='localhost',
     database='north',
     user='postgres',
     password='***'
-) as conn:
-    with conn.cursor() as curs:
+)
+try:
+    with conn:
+        with conn.cursor() as curs:
 
-        curs.executemany('INSERT INTO customers VALUES (%s, %s, %s)', customers)
-        curs.execute('SELECT * FROM customers')
+            curs.executemany('INSERT INTO customers VALUES (%s, %s, %s)', customers)
+            curs.execute('SELECT * FROM customers')
 
-        curs.executemany('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', employees)
-        curs.execute('SELECT * FROM employees')
+            curs.executemany('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', employees)
+            curs.execute('SELECT * FROM employees')
 
-        curs.executemany('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', orders)
-        curs.execute('SELECT * FROM orders')
+            curs.executemany('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', orders)
+            curs.execute('SELECT * FROM orders')
 
-        conn.commit()
+            conn.commit()
+
+finally:
+    conn.close()
